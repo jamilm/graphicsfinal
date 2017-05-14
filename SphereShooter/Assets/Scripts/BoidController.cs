c2131e2a237e0ab376ff4f73ecb91bf51c1e45af
dxyang <daniel.yang17@gmail.com>
2017-05-13 22:07:28 -0400

    Flocking basic behavior merged in. Needs to be tuned. Feel free to revert.

---
+++ SphereShooter/Assets/Scripts/BoidController.cs
@@ -0,0 +1,104 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BoidController : MonoBehaviour
+{
+	public float minVelocity = 0;
+	public float maxVelocity = 0.01f;
+	public float randomness = 1;
+	public int flockSize = 20;
+	public GameObject prefab;
+	public GameObject chasee;
+
+	public Vector3 flockCenter;
+	public Vector3 flockVelocity;
+
+	public GameObject[] boids;
+
+	public float radius = 0.5f;
+	public float cx = 0;
+	public float cy = 0;
+	public float cz = 0;
+
+	void Start()
+	{
+		boids = new GameObject[flockSize];
+		for (var i=0; i<flockSize; i++)
+		{
+			float z = Random.Range (-radius, radius); 
+			float phi = Random.Range (0, 2 * Mathf.PI);
+			float d = Mathf.Sqrt (radius * radius - z * z);
+
+			float px = cx + d * Mathf.Cos (phi);
+			float py = cy + d * Mathf.Sin (phi);
+			float pz = cz + z; 
+
+			Vector3 randomPosition = new Vector3 (px, py, pz); 
+			Quaternion randomRotation = Random.rotation;
+
+			GameObject boid = Instantiate(prefab, randomPosition, randomRotation) as GameObject;
+			boid.transform.parent = transform.parent;
+			boid.transform.localPosition = randomPosition;
+			boid.GetComponent<BoidFlocking>().SetController (gameObject);
+//			Debug.Log ("Set Controller");
+			boids[i] = boid;
+		}
+	}
+
+	void Update ()
+	{
+		Vector3 theCenterSpherical = Vector3.zero;
+		Vector3 theCenter = Vector3.zero;
+		Vector3 theVelocity = Vector3.zero;
+
+		foreach (GameObject boid in boids)
+		{
+			Vector3 boidCenterEuclid = boid.transform.localPosition;
+			Vector3 boidCenterSpherical = EuclideanToSpherical (boidCenterEuclid);
+			theCenterSpherical = theCenterSpherical + boidCenterSpherical;
+
+			//theCenter = theCenter + boid.transform.localPosition;
+			theVelocity = theVelocity + boid.GetComponent<Rigidbody>().velocity;
+		}
+
+		// convert center spherical to euclidean
+		theCenterSpherical = theCenterSpherical/(flockSize);
+		Vector3 centerEuclid = SphericalToEuclidean (theCenterSpherical);
+
+		// update values
+		flockCenter = centerEuclid;
+		//flockCenter = theCenter/(flockSize);
+		flockVelocity = theVelocity/(flockSize);
+
+		Debug.DrawRay(flockCenter, flockCenter*100.0f, Color.red);
+	}
+
+	// returns spherical vector as r, theta, phi
+	private Vector3 EuclideanToSpherical(Vector3 euclid)
+	{
+		float x = euclid.x;
+		float y = euclid.y;
+		float z = euclid.z;
+
+		float r = Mathf.Sqrt (x * x + y * y + z * z);
+		float theta = Mathf.Acos (z / r);
+		float phi = Mathf.Atan (y / x);
+
+		print (new Vector3 (r, theta, phi));
+		return new Vector3 (r, theta, phi);
+	}
+
+	// assumes spherical vector is r, theta, phi
+	private Vector3 SphericalToEuclidean(Vector3 spherical)
+	{
+		float r = spherical.x;
+		float theta = spherical.y;
+		float phi = spherical.z;
+
+		float x = r * Mathf.Sin (theta) * Mathf.Cos (phi);
+		float y = r * Mathf.Sin (theta) * Mathf.Sin (phi);
+		float z = r * Mathf.Cos (theta);
+
+		return new Vector3 (x, y, z);
+	}
+}
---
+++ SphereShooter/Assets/Scripts/BoidFlocking.cs
@@ -0,0 +1,100 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BoidFlocking : MonoBehaviour
+{
+	private GameObject Controller;
+	private bool inited = false;
+	private float minVelocity;
+	private float maxVelocity;
+	private float randomness;
+	private GameObject chasee;
+	private Rigidbody rigidbody; 
+
+	public Rigidbody earth; 
+	public float radius; 
+	void Start ()
+	{
+		rigidbody = GetComponent<Rigidbody> ();
+//		StartCoroutine ("BoidSteering");
+	}
+		
+	private Vector3 Calc ()
+	{
+		Vector3 randomize = new Vector3 ((Random.value *2) -1, (Random.value * 2) -1, (Random.value * 2) -1);
+
+		randomize.Normalize();
+
+		// get flock stats
+		BoidController boidController = Controller.GetComponent<BoidController>();
+		Vector3 flockCenter = boidController.flockCenter;
+		Vector3 flockVelocity = boidController.flockVelocity;
+		Vector3 follow = chasee.transform.position;
+
+		// cohesion/separation
+		flockCenter = flockCenter - transform.position;
+		float flockScale = 1.0f / (flockCenter.magnitude * flockCenter.magnitude);
+		flockCenter.Normalize ();
+
+		// alignment
+		flockVelocity = flockVelocity - rigidbody.velocity;
+		flockVelocity.Normalize ();
+
+		// follow
+		follow = follow - transform.position;
+		follow.Normalize ();
+
+		// separation
+//		separation = separation * -1f;
+//		Vector3 separation = new Vector3(0,0,0);
+//		GameObject[] boids = boidController.boids;
+//		for (int i = 0; i < boids.Length; i++) {
+//			float distance = Vector3.Distance (transform.position, boids[i].transform.position);
+//			Debug.Log (distance);
+//			if (distance < 1f) { 
+//				separation -= boidController.boids [i].transform.position;
+//			}
+//		}
+		return (flockCenter + flockVelocity + follow * 10.0f  + randomize * randomness);
+	}
+
+	public void SetController (GameObject theController)
+	{
+		Controller = theController;
+		BoidController boidController = Controller.GetComponent<BoidController>();
+		minVelocity = boidController.minVelocity;
+		maxVelocity = boidController.maxVelocity;
+		randomness = boidController.randomness;
+		chasee = boidController.chasee;
+		inited = true;
+	}
+
+	public void Update() {
+		if (inited) {
+
+			Vector3 normal = (transform.position - earth.position).normalized;
+
+			Vector3 velocity = rigidbody.velocity + Calc () * Time.deltaTime;
+			rigidbody.velocity = Vector3.ProjectOnPlane (velocity, normal);
+
+			Debug.DrawRay (rigidbody.position, rigidbody.velocity * 2.0f);
+
+			// enforce minimum and maximum speeds for the boids
+			float speed = rigidbody.velocity.magnitude;
+			if (speed > maxVelocity) {
+				rigidbody.velocity = rigidbody.velocity.normalized * maxVelocity;
+			} else if (speed < minVelocity) {
+				rigidbody.velocity = rigidbody.velocity.normalized * minVelocity;
+			}
+
+			Vector3 dist = rigidbody.position - earth.position;
+			if (dist.magnitude > 0.5f) {
+				dist.Normalize ();
+				dist *= 0.5f;
+				rigidbody.position = dist;
+			}
+		}
+
+
+	}
+}
